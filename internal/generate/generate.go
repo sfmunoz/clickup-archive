bd3ef2069ac4f3363ded21b32b0d728e3526f17a
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-03-31 15:32:35 +0000

    hugo related stuff dropped: bubbletea based tui will be used instead

--- internal/generate/generate.go
+++
@@ -1,7 +0,0 @@
-package generate
-
-import (
-	"github.com/sfmunoz/logit"
-)
-
-var log = logit.Logit().WithLevel(logit.LevelInfo)
--- internal/hugo/hugo.go
+++
@@ -1,7 +0,0 @@
-package hugo
-
-import (
-	"github.com/sfmunoz/logit"
-)
-
-var log = logit.Logit().WithLevel(logit.LevelInfo)
