2c6dc537622cee43588b97ad75b95f617ea04ad1
sfmunoz <46285520+sfmunoz@users.noreply.github.com>
2026-04-03 16:31:09 +0000

    tui: stats provided with some style

--- internal/tui/stats.go
+++ internal/tui/stats.go
@@ -5,8 +5,16 @@ import (
 	"strings"
 
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 	"github.com/sfmunoz/clickup-archive/internal/archive"
 )
+
+var statsStyle = lipgloss.NewStyle().
+	AlignHorizontal(lipgloss.Center).
+	Border(lipgloss.RoundedBorder(), true).
+	Padding(1, 5)
+
+var statsTitleStyle = lipgloss.NewStyle().Bold(true).Underline(true)
 
 type Stats struct {
 	archive *archive.Archive
@@ -54,11 +62,12 @@ func (s *Stats) View() string {
 		}
 	}
 	var sb strings.Builder
+	fmt.Fprint(&sb, statsTitleStyle.Render("stats")+"\n\n")
 	fmt.Fprintf(&sb, "workspaces ... %4d\n", wTot)
 	fmt.Fprintf(&sb, "spaces ....... %4d\n", sTot)
 	fmt.Fprintf(&sb, "folders ...... %4d\n", fTot)
 	fmt.Fprintf(&sb, "lists ........ %4d\n", lTot)
 	fmt.Fprintf(&sb, "tasks ........ %4d\n", tTot)
-	fmt.Fprintf(&sb, "comments ..... %4d\n", cTot)
-	return sb.String()
+	fmt.Fprintf(&sb, "comments ..... %4d", cTot)
+	return statsStyle.Render(sb.String())
 }
